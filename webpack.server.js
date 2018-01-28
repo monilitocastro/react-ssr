2be1711e326796e50f908cc80d044fb6c35142be
Monilito Castro <mannycastro.epic@gmail.com>
2018-01-27 23:55:18 -0600

    refactored webpack to share baseConfig and for all three dev scripts to be run by one command

--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,8 @@
 const path = require("path");
+const merge = require("webpack-merge");
+const baseConfig = require("./webpack.base.js");
 
-module.exports = {
+const config = {
   // inform webpack we're building bundle for nodejs
   // rather than the browser
   target: "node",
@@ -13,24 +15,7 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "build")
-  },
-
-  // tell webpack to run babel on every file
-  // it runs through
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        loader: "babel-loader",
-        exclude: /node_modules/,
-        options: {
-          presets: [
-            "react",
-            "stage-0",
-            ["env", { targets: { browsers: ["last 2 versions"] } }]
-          ]
-        }
-      }
-    ]
   }
 };
+
+module.exports = merge(baseConfig, config);
