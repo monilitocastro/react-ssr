d2aed403267c547d43e824a44f653f642dd56300
Monilito Castro <mannycastro.epic@gmail.com>
2018-01-28 00:39:18 -0600

    added webpack-node-externals and refactored presentation away from server functions

--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const merge = require("webpack-merge");
 const baseConfig = require("./webpack.base.js");
+const webpackNodeExternals = require("webpack-node-externals");
 
 const config = {
   // inform webpack we're building bundle for nodejs
@@ -15,7 +16,8 @@ const config = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "build")
-  }
+  },
+  externals: [webpackNodeExternals()]
 };
 
 module.exports = merge(baseConfig, config);
